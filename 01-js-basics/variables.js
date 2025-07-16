b1c76d53bd7aa94938cedb3739136124e01cd7c0
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-16 14:25:58 +0300

    add variables usage

---
+++ 01-js-basics/variables.js
@@ -0,0 +1,7 @@
+const meyveler = ["elma", "muz"];
+meyveler.push("portakal");
+console.log(meyveler);
+
+//const ile tanımlanan değişkenlerin değerini değiştiremeyiz. ama diziyse veya objeyse içindeki değerleri değiştirebiliriz.
+
+//eleman eklemek için, elemanı sona ekler
---
+++ examples.js
@@ -0,0 +1,7 @@
+console.log("log");
+console.warn("warn");
+console.error("error");
+
+const isim = "Rana ve Şeyma";
+const sehir = "İstanbul";
+const mesaj = `Merhaba ${isim}`;
