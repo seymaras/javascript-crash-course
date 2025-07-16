619bffb1f336ded8799e4b2d46a08910f224ceab
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-16 17:56:24 +0300

    Update data-types-string.js

--- 01-js-basics/data-types-string.js
+++ 01-js-basics/data-types-string.js
@@ -42,3 +42,50 @@ console.log(isim.slice(-5));
 console.log(
   isim.toLocaleLowerCase().includes(searchString.toLocaleLowerCase().trim())
 );
+
+const text1 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non velit eget massa mollis dictum tincidunt nec mi. Aenean porta ante vitae turpis consectetur tempor. Fusce eget ex et lectus euismod consectetur. Curabitur nec ultricies lacus. Sed et quam tortor. Vestibulum imperdiet lobortis scelerisque. Quisque et nulla justo. Mauris erat leo, hendrerit in felis vitae, efficitur auctor velit. Fusce placerat fringilla malesuada.";
+
+console.log(text1.split(" ").length);
+
+//Örnek- Kelime Sayacı
+const cumle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ";
+const kelimeler = cumle.trim().split(" ");
+console.log(`kelime sayisi: ${kelimeler.length}`);
+
+//Örnek-İlk Harfleri Büyük Yap
+const metin1 = "hava bugün cOK güzel";
+const kelimeler1 = metin1.split(" ");
+
+const guncelKelimeler = kelimeler1.map((value) => {
+  return value.at(0).toUpperCase() + value.slice(1).toLowerCase(); //slice(1) bir den itibaren tüm harfleri alır.
+});
+const guncelMetin = guncelKelimeler.join(" ");
+
+console.log(guncelMetin);
+//--------------------------------
+const metin3 = "en sevdigim tatli sütlac";
+
+const kelimeler3 = metin3.split(" ");
+
+const guncelk = kelimeler3.map((value) => {
+  return value.at(0).toUpperCase() + value.slice(1).toLowerCase();
+});
+console.log(guncelk);
+
+const guncelmetin1 = guncelk.join(" ");
+console.log(guncelmetin1);
+
+//ornek- tersine cevir
+
+const kelime = "cicek";
+
+const kelimebol = kelime.split("");
+
+const tersi = kelimebol.reverse().join("");
+
+console.log(tersi);
+
+//ornek palindrome kontrolü - Bir kelimenin tersten de aynı olup olmadığını kontrol eden kod:
+
+const kelime4 = "kayak";
