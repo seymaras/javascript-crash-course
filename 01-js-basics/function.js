631245dae98c93ca27c0a9cb2e22a72048cebb49
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-18 18:35:05 +0300

    Create function.js

---
+++ 01-js-basics/function.js
@@ -0,0 +1,23 @@
+//const fonsiyonAdi = (parametre)=>{
+//islemler
+//    return sonuc
+//}
+
+const selamla = (isim) => {
+  console.log(`merhaba ${isim}!`);
+};
+selamla("Rana");
+
+const topla = (a, b) => {
+  return a + b;
+};
+console.log(topla(3, 4));
+
+const kare = (sayi) => {
+  return sayi * sayi;
+};
+
+//return varsa, bu fonksiyon bir sonuç döner
+//return yoksa, sadece işlemi yapar ama bir değer dönmez (undefined olur)
+
+//TEK SATIRLIK ARROW FUNCTION
