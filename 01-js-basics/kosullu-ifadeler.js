19c612a59bbf418d5e2fd37ece36b4081b43d53e
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-18 11:05:14 +0300

    Update kosullu-ifadeler.js

--- 01-js-basics/kosullu-ifadeler.js
+++ 01-js-basics/kosullu-ifadeler.js
@@ -38,3 +38,49 @@ if (not >= 90) {
 //| `&&`     | VE – ikisi de doğruysa | `yas > 18 && uyruk === "TR"` |                           |            |   |                  |
 //| \`       |                        | \`                           | VEYA – biri bile doğruysa | \`not > 90 |   | bonus === true\` |
 //| `!`      | DEĞİL – tersini alır   | `!girisYapildiMi`            |                           |            |   |                  |
+
+// yas kontrolu
+
+const yas2 = 16;
+
+if (yas2 < 0) {
+  console.log("gecersiz yas");
+} else if (yas2 >= 0 && yas2 <= 12) {
+  console.log("cocuk");
+} else if (yas2 >= 13 && yas2 <= 17) {
+  console.log("ergen");
+} else {
+  console.log("yetişkin");
+}
+
+//sayı pozitif mi negatif mi
+const sayi3 = -4;
+
+if (sayi3 > 0) {
+  console.log("pozitif");
+} else {
+  console.log("negatif");
+}
+
+//switch - Genellikle çok sayıda eşitlik karşılaştırması (örn. if (x === ...)) yapacağımız zaman kullanılır.
+
+const gun = "pazartesi";
+
+switch (gun) {
+  case "pazartesi":
+    console.log("haftanın ilk günü");
+    break;
+  case "cuma":
+    console.log("haftasonuna az kaldı");
+    break;
+  case "pazar":
+    console.log("yarın pazartesi :(");
+    break;
+
+  default:
+    console.log("sıradan bir gün");
+}
+//Eğer break yazmazsak, eşleşen case’den sonraki tüm case’ler çalışmaya devam eder.
+//default ne işe yarar?
+// - Hiçbir case eşleşmezse devreye girer.
+// - else gibidir
