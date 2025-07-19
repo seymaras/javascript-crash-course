c2e32f16dd642deb3454b5129703fada276a0d95
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-19 12:06:23 +0300

    Update function.js

--- 01-js-basics/function.js
+++ 01-js-basics/function.js
@@ -59,8 +59,77 @@ const carpim = (a, b) => {
 console.log(`carpimi: ${carpim(a, b)}`); //fonksiyonu cagırmak icin : ${carpim(a, b)
 
 //yashesapla
-const yashesapla = (dogumYil) => {
+const yasHesapla = (dogumYili) => {
   const simdikiYil = new Date().getFullYear();
-  return simdikiYil - dogumYil;
+  return simdikiYil - dogumYili;
 };
-console.log(`Yasiniz: ${yashesapla(2002)}`);
+console.log(`yasınız: ${yasHesapla(2002)}`);
+
+//metin kisa mi
+
+const kisaMi = (metin) => {
+  return metin.length < 10;
+};
+console.log(kisaMi("Merhaba"));
+console.log(kisaMi("Selam millet!"));
+// metin kısa mı - tek satırlık hali
+const kisaMi2 = (metin) => metin.length < 10;
+console.log(kisaMi2("Selam millet!"));
+
+// en buyuk sayiyi bulur
+const enBuyuk = (a, b, c) => {
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
+};
+console.log(enBuyuk(10, 5, 8));
+console.log(enBuyuk(3, 12, 9));
+console.log(enBuyuk(7, 7, 7));
+
+//yukarıda ki kodu tek satırlık yapma
+
+const enBuyuk2 = (a, b, c) => Math.max(a, b, c);
+//Math.max() → içine verdiğiniz sayılardan en büyüğünü döner
+//return gerekmez çünkü tek satırda direkt sonuç veriyor
+
+//----------------------Örnekler---------------------------------
+
+const selamla1 = (isim3) => {
+  console.log(`Merhaba ${isim3}`);
+};
+selamla1("seyma");
+
+//kare alma
+
+const kareal = (sayi) => {
+  karelisayi = sayi * sayi;
+  console.log(`sayinin karesi: ${karelisayi}`);
+};
+kareal(10);
+
+//kare almanınn daha kısa yolu
+
+const kareal2 = (sayi2) => {
+  return sayi2 * sayi2;
+};
+console.log(`sayinin karesi: ${kareal2(4)}`);
+
+//metin uzunluğu
+
+const uzunluk = (metin) => {
+  return metin.length < 10;
+};
+console.log(uzunluk("ben seyma kahveyi seviyorum"));
+
+// buyuk harf yapma
+
+const buyukHarf = (isim4) => {
+  return isim4.toUpperCase();
+};
+console.log(buyukHarf("kahveyi seviyorum"));
+
+//
