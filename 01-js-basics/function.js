0e63b441e21b27677e6de17f2b8f6b9cfe2b71c5
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-19 11:15:04 +0300

    Update function.js

--- 01-js-basics/function.js
+++ 01-js-basics/function.js
@@ -5,6 +5,7 @@
 
 const selamla = (isim) => {
   console.log(`merhaba ${isim}!`);
+  const selamla = (isim) => console.log("Merhaba " + isim);
 };
 selamla("Rana");
 
@@ -21,3 +22,45 @@ const kare = (sayi) => {
 //return yoksa, sadece işlemi yapar ama bir değer dönmez (undefined olur)
 
 //TEK SATIRLIK ARROW FUNCTION
+//Eğer fonksiyon sadece bir işlem yapacaksa ve tek satırsa, {} ve return yazmaya gerek yok
+
+const kare1 = (x) => x * x;
+//const selamla = (isim) => console.log("Merhaba " + isim);
+
+//hic parametre yoksa
+const hosgeldin = () => console.log("Hoş geldiniz!");
+
+// fonksiyon icinde kontrol
+const ciftMİ = (sayi) => {
+  if (sayi % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//üstünde kinin kısa hali
+const ciftMi = (sayi) => sayi % 2 === 0;
+
+//fonksiyon icinde fonksiyon
+const hesapla = (a, b) => {
+  const carp = (x, y) => x * y;
+  return carp(a, b);
+};
+console.log(hesapla(3, 4));
+
+//carpım
+
+const a = 3;
+const b = 5;
+const carpim = (a, b) => {
+  return a * b;
+};
+console.log(`carpimi: ${carpim(a, b)}`); //fonksiyonu cagırmak icin : ${carpim(a, b)
+
+//yashesapla
+const yashesapla = (dogumYil) => {
+  const simdikiYil = new Date().getFullYear();
+  return simdikiYil - dogumYil;
+};
+console.log(`Yasiniz: ${yashesapla(2002)}`);
