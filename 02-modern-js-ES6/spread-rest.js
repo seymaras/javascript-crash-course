efc8946f046c87e4729b88e3a344604a3d929078
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-21 15:15:24 +0300

    Create spread-rest.js

---
+++ 02-modern-js-ES6/spread-rest.js
@@ -0,0 +1,44 @@
+//| Operatör   | Anlamı     | Ne Yapıyor?                             | Kullanıldığı Yer                     |
+//| ---------- | ---------- | --------------------------------------- | ------------------------------------ |
+//| **Spread** | yaymak     | Diziyi ya da objeyi **açıyor**          | Atama, birleştirme                   |
+//| **Rest**   | toparlamak | Birden çok değeri **tek diziye toplar** | Fonksiyon parametresi, destructuring |
+
+//spread - ...(yayma)
+
+const dizi1 = [1, 2];
+const dizi2 = [3, 4];
+const birlesik = [...dizi1, ...dizi2];
+
+console.log(birlesik);
+
+// obje birlestirme
+
+const kisi = { ad: "Rana" };
+const bilgiler = { yas: 23, sehir: "Konya" };
+const yeniKisi = { ...kisi, ...bilgiler };
+
+console.log(yeniKisi);
+
+// kopyamlama
+
+const orijinal = [10, 20];
+const kopya = [...orijinal];
+
+console.log(kopya);
+
+//rest - ...(toplama)
+
+const toplama = (...sayilar3) => {
+  console.log(sayilar3);
+  return sayilar3.reduce((a, b) => a + b);
+};
+console.log(toplama(5, 10, 15));
+
+// kalanları bir araya getirir
+
+const [ilk, ...digerleri] = [1, 2, 3, 4];
+
+console.log(ilk);
+console.log(digerleri);
+
+// const user
