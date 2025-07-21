9a6960db9203602272cf1a7854e1d31200718081
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-21 10:23:59 +0300

    Create array.destructuring.js

---
+++ 02-modern-js-ES6/array.destructuring.js
@@ -0,0 +1,15 @@
+const sayilar = [1, 2, 3];
+const [a, b, c] = sayilar;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//ilk elemznı al
+
+const renkler = ["mavi", "pembe", "sari", "kirmizi"];
+
+const [ilk, ikinci] = renkler;
+
+console.log(ilk);
+console.log(ikinci);
