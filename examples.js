113f247d658d9746a2e09fb8b9f92f9d1a0a1e18
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-21 10:24:08 +0300

    Update examples.js

--- examples.js
+++ examples.js
@@ -5,3 +5,16 @@ console.error("error");
 const isim = "Rana ve Şeyma";
 const sehir = "İstanbul";
 const mesaj = `Merhaba ${isim}`;
+
+//
+
+const kisi = {
+  isim: "Rana",
+  yas: 23,
+  okul: "ytü",
+};
+
+function kisiBilgisi({ isim, yas, okul }) {
+  console.log(`${isim}, ${yas} yasıında`);
+}
+kisiBilgisi(kisi);
