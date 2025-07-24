e6d50423962a7c7e535fac847f508501a40132b6
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-07-24 19:16:56 +0300

    Create single-elements-script.js

---
+++ 04-DOM-manipulation-event-handling/single-elements-script.js
@@ -0,0 +1,39 @@
+let sonuc;
+
+/*
+sonuc = document.getElementById("title");
+sonuc = document.getElementById("title").id;
+sonuc = document.getElementById("title").className;
+sonuc = document.getElementById("title").classList;
+
+document.getElementById("title").style.fontSize = "50px";
+document.getElementById("title").style.color = "pink";
+document.getElementById("title").style.classList;
+
+document.getElementById("title").style.fontSize = "50px";
+document.getElementById("title").style.color = "pink";
+//document.getElementById("title").style.display = "none"; - sayfada görünmez ama konsolda görnünür
+
+document.getElementById("title").innerText = "Todo App";
+document.getElementById("title").innerHTML = "<span>Todo App</span>"; */
+
+/* sonuc = document.querySelector("#title"); // #-id nokta ile de class
+sonuc = document.querySelector(".card-header");
+sonuc = document.querySelector("div");
+
+sonuc = document.querySelector("li");
+sonuc = document.querySelector("li:first-child");
+sonuc = document.querySelector("li:last-child"); */
+
+sonuc = document.querySelector("li");
+sonuc = document.querySelector("ul li");
+sonuc = document.querySelector("ol li");
+sonuc = document.querySelector("ol li:last-child");
+sonuc = document.querySelector("ul li:last-child");
+
+sonuc = document.getElementById("title");
+sonuc = document.querySelector("#title");
+
+sonuc = document.querySelector(".card-header");
+
+console.log(sonuc);
