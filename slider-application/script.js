d1117259db0af7d5befcd1c5cae880d8ab9b6930
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-08-07 06:13:23 +0300

    slide-controls

--- slider-application/script.js
+++ slider-application/script.js
@@ -26,7 +26,38 @@ var models = [
   },
 ];
 
-var index = 1;
+var index = 2;
+var slaytCount = models.length;
+document.querySelector(".fa-arrow-left").addEventListener("click", function() {
+  index--;
+  showSlide(index);
+  console.log(index);
+});
+document.querySelector(".fa-arrow-right").addEventListener("click", function() {
+  index++;
+  showSlide(index);
+  console.log(index);
+});
+function showSlide(i) {
+  index = i;
+  if (i < 0) {
+    index = slaytCount - 1;
+  }
+  if (i >= slaytCount) {
+    index = 0;
+  }
+  if (i >= slaytCount) {
+    index = 0;
+  }
+
+  document.querySelector(".card-title").textContent = models[index].name;
+
+  document
+    .querySelector(".card-img-top")
+    .setAttribute("src", models[index].image);
+  document.querySelector(".card-link").setAttribute("href", models[index].link);
+}
+
 document.querySelector(".card-title").textContent = models[index].name;
 document
   .querySelector(".card-img-top")
