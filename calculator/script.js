5492442703940808af1be6d3dd0a06a1f288de1e
Asiye Şeyma Aras <seyma02aras@gmail.com>
2025-08-09 19:39:50 +0300

    perform-calculation

--- calculator/script.js
+++ calculator/script.js
@@ -2,6 +2,9 @@ const display = document.querySelector(".calculator-input");
 const keys = document.querySelector(".calculator-keys");
 
 let displayValue = "0";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 function updateDisplay() {
   display.value = displayValue;
@@ -12,7 +15,9 @@ keys.addEventListener("click", function(e) {
   if (!element.matches("button")) return;
 
   if (element.classList.contains("operator")) {
-    console.log("operator", element.value);
+    //console.log("operator", element.value);
+    handleOperator(element.value);
+    updateDisplay();
     return;
   }
   if (element.classList.contains("decimal")) {
@@ -34,8 +39,15 @@ keys.addEventListener("click", function(e) {
 });
 
 function inputNumber(num) {
-  displayValue = displayValue === "0" ? num : displayValue + num;
+  if (waitingForSecondValue) {
+    displayValue = num;
+    waitingForSecondValue = false;
+  } else {
+    displayValue = displayValue === "0" ? num : displayValue + num;
+  }
+  console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
+
 function inputDecimal() {
   if (!displayValue.includes(".")) {
     displayValue += ".";
@@ -43,4 +55,37 @@ function inputDecimal() {
 }
 function clear() {
   displayValue = "0";
+}
+
+function calculate(first, second, operator) {
+  if (operator === "+") {
+    return first + second;
+  } else if (operator === "-") {
+    return first - second;
+  } else if (operator === "*") {
+    return first * second;
+  } else if (operator === "/") {
+    return first / second;
+  }
+  return second;
+}
+
+function handleOperator(nextOperator) {
+  const value = parseFloat(displayValue);
+  if (operator && waitingForSecondValue) {
+    operator = nextOperator;
+    return;
+  }
+
+  if (firstValue === null) {
+    firstValue = value;
+  } else if (operator) {
+    const result = calculate(firstValue, value, operator);
+    displayValue = `${parseFloat(result.toFixed(7))}`;
+    firstValue = result;
+  }
+  waitingForSecondValue = true;
+  operator = nextOperator;
+
+  console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
